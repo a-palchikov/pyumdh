0332f517b0582e4054eef2e6b651a6190d2bd02c
Anton <deemok@gmail.com>
2012-04-29 23:57:46 +0200

    * empty config

--- pyumdh/config.py
+++ pyumdh/config.py
@@ -7,17 +7,19 @@ import re
 # with that of debugging tools (32bits vs 64bits) - these must match for
 # memory snapshots to work
 # 32bits
-DBG_TOOLS_PATH = 'd:\\vpdev\\dbgrs\\windbg'
+# DBG_TOOLS_PATH = 'path\\to\\debugging\\tools\\for\\windows'
 # 64bits
 #DBG_TOOLS_PATH=r'C:\Program Files\Debugging Tools for Windows (x64)'
+DBG_TOOLS_PATH = ''
 
 # List of symbol paths
 # You can also include symbol server style paths if symsrv.dll is available
-DBG_SYMBOL_PATHS = ['p:\\development\\dumpdump\\PDBs\\6.2.0#1300', \
-                'd:\\vpdev\\Piant_6\\ExtLib\\Qt\\lib', \
-                'srv*d:\\vpdev\\websymbols*http://msdl.microsoft.com/download/symbols']
+#DBG_SYMBOL_PATHS = ['srv*path\\to\\symbol\\store*http://msdl.microsoft.com/download/symbols']
+DBG_SYMBOL_PATHS = []
+
 # Path to binaries used by symbol provider
-DBG_BIN_PATHS = [r'D:\VPProducts\ViewPoint_1300\VPCommon']
+#DBG_BIN_PATHS = ['path\\to\\your\\application']
+DBG_BIN_PATHS = []
 
 # If True, will automatically launch analysis session when two snapshots
 # are available
@@ -32,6 +34,7 @@ WORK_DIR = 'data'
 # Stack frames with these patterns matched will be skipped from diff/dump
 # Note, the setting is crrent effective only in backtrace.py when it's
 # used as main program
+# FIXME Replace with patterns that suit your needs before using it!
 TRUSTED_PATTERNS = [re.compile(r'qtgui4!qfontenginewin::recalcAdvances', \
                         re.IGNORECASE), \
                     re.compile(r'qtcore4!_hb_alloc', re.IGNORECASE), \
@@ -42,4 +45,5 @@ TRUSTED_PATTERNS = [re.compile(r'qtgui4!qfontenginewin::recalcAdvances', \
 
 # These are modules making allocations that we choose to skip during diff/dump
 # If specified, these take precedence over TRUSTED_PATTERNS
+# FIXME Replace with patterns that suit your needs before using it!
 TRUSTED_MODULES = ['dbghelp.dll']
